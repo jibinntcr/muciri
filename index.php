e6305c79ac5de81de5a1019757551f7a1beda951
jerry sabu <jerry@lanwaresolutions.com>
2023-03-01 17:23:21 +0530

    about added

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
         </div>
         <!-- slider Area End-->
         <!-- Our Services Start -->
-        <div class="our-services servic-padding">
+        <!-- <div class="our-services servic-padding">
             <div class="container">
                 <div class="row d-flex justify-contnet-center">
                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">
@@ -110,8 +110,71 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- Our Services End -->
+
+
+
+
+
+        <!-- about started -->
+        <div class="support-company-area servic-padding fix">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="support-location-img mb-50">
+                            <img src="assets/img/service/support-img.jpg" alt="" />
+                            <div class="support-img-cap">
+                                <span>Since 1992</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="right-caption">
+                            <!-- Section Tittle -->
+                            <div class="section-tittle section-tittle2">
+                                <span>About Our Company</span>
+                                <h2>We are Go Trip <br />Ravels Support Company</h2>
+                            </div>
+                            <div class="support-caption">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                                    aliqua. Ut enim ad minim veniam, quis nostrud
+                                </p>
+                                <div class="select-suport-items">
+                                    <label class="single-items">Lorem ipsum dolor sit amet
+                                        <input type="checkbox" checked="checked active" />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <label class="single-items">Consectetur adipisicing sed do
+                                        <input type="checkbox" checked="checked active" />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <label class="single-items">Eiusmod tempor incididunt
+                                        <input type="checkbox" checked="checked active" />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                    <label class="single-items">Ad minim veniam, quis nostrud.
+                                        <input type="checkbox" checked="checked active" />
+                                        <span class="checkmark"></span>
+                                    </label>
+                                </div>
+                                <a href="#" class="btn border-btn">Search</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- about end  -->
+
+
+
+
+
+
+
         <!-- Favourite Places Start -->
         <div class="favourite-place place-padding">
             <div class="container">
